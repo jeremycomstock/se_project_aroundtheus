ebf39c88d74bc33bce52143f2c294ede1f9429cc
JeremyComstock <comstjer@gmail.com>
2023-10-27 19:36:29 -0600

    Add script to control modal box

--- scripts/index.js
+++ scripts/index.js
@@ -28,13 +28,30 @@ const initialCards = [
 const editButton = document.querySelector(".profile__edit-button");
 const modalBox = document.querySelector(".modal");
 const closeButton = document.querySelector(".modal__close");
+const saveButton = modalBox.querySelector(".modal__button");
+let modalInputName = modalBox.querySelector("#title");
+let modalInputDescription = modalBox.querySelector("#description");
+const profile = document.querySelector(".profile");
+let profileName = profile.querySelector(".profile__name");
+let profileDescription = profile.querySelector(".profile__description");
 
 function openEditor() {
   modalBox.setAttribute("style", "display: flex");
+  modalInputName.value = profileName.textContent;
+  modalInputDescription.value = profileDescription.textContent;
 }
+
+function saveProfile(e) {
+  profileName.textContent = modalInputName.value;
+  profileDescription.textContent = modalInputDescription.value;
+  e.preventDefault();
+}
+
 function closePopup() {
   modalBox.setAttribute("style", "display: none");
 }
 
 editButton.addEventListener("click", openEditor);
 closeButton.addEventListener("click", closePopup);
+saveButton.addEventListener("click", saveProfile);
+saveButton.addEventListener("click", closePopup);
